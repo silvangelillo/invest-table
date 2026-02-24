c7241dfcc74772fc3f8e76bafefa561b126197c4
SilvioXR <silvio.angelillo@xr-eality.de>
2026-02-24 20:42:10 +0100

    fix: next.config syntax

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   turbopack: {},
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**.supabase.co" },
